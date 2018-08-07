2ae7fd377ce916ee8f00e799b776823f36e7c882
manoharanRajesh <mano.rajesh@gmail.com>
2018-08-08 00:33:06 +0200

    initial commit for deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+  MdcButtonModule,
+  MdcFabModule,
+  MdcIconModule,
+  MdcToolbarModule,
+  MdcCardModule,
+  MdcElevationModule,
+  MdcRippleModule,
+  MdcListModule,
+    MdcIconButtonModule,
+    MdcIconToggleModule,
+    MdcTextFieldModule,
+    MdcThemeModule,
+    MdcTypographyModule,
+    MdcShapeModule,
+    MdcCheckboxModule,
+} from '@angular-mdc/web';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MdcCheckboxModule,
+    MdcListModule,
+    MdcShapeModule,
+    MdcCardModule,
+    MdcButtonModule,
+    MdcIconModule,
+    MdcIconButtonModule,
+    MdcIconToggleModule,
+    MdcRippleModule,
+    MdcTextFieldModule,
+    MdcListModule,
+    MdcThemeModule,
+    MdcTypographyModule,
+    MdcListModule,
+    MdcRippleModule,
+    MdcElevationModule,
+    MdcCardModule,
+    MdcIconModule,
+    MdcToolbarModule,
+   MdcButtonModule,
+   MdcFabModule,
+   MdcIconModule,
+   BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

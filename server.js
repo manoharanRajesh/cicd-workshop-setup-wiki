778d44607a8ca1fb80d4ae0bec4a77541da47261
manoharanRajesh <mano.rajesh@gmail.com>
2018-08-08 00:52:50 +0200

    app deploy test

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+app .get('/', (req, res) => res.render('PROJECT-NAME/index'))
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
